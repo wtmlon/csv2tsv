90e199547d5dc8e82a3a91c2d089a9368300aca3
西瓜 <wtmlon@foxmail.com>
2021-05-18 22:34:08 +0800

    #去除双引号

--- csv2tsv.py
+++ csv2tsv.py
@@ -4,6 +4,7 @@ import re
 
 def process(x):
     x = re.sub(r'([?.,!])$',r' \1', x)
+    x = re.sub(r'["“”]',r'', x)
     return x
         
 if __name__ == '__main__':
@@ -20,10 +21,10 @@ if __name__ == '__main__':
         print(name, counsel[name])
         pd_name = pd.read_csv(counsel[name])
         pd_name = pd.DataFrame(pd_name, columns=['questionTitle', 'answerText'])
-        pd_name = pd_name.replace(r'"', r'', regex=True)
-        pd_name['questionTitle'] = '0.0 '+ pd_name['questionTitle'].astype(str)
+        #pd_name = pd_name.replace(r'["“”]', '', regex=True)    # 去除双引号
+        pd_name['questionTitle'] = '0.0 '+ pd_name['questionTitle'].astype(str) #Series每行首拼接字符
         pd_name['answerText'] = '1.0 '+ pd_name['answerText'].astype(str)
-        pd_name['questionTitle'] = pd_name['questionTitle'].apply(process)
+        pd_name['questionTitle'] = pd_name['questionTitle'].apply(process)  #对Seies每行aly
         pd_name['answerText'] = pd_name['answerText'].apply(process)
         print(pd_name)
         pd_name.to_csv('./Data/'+name+'.tsv', sep='\t', index=False)
@@ -32,7 +33,7 @@ if __name__ == '__main__':
         print(name, scrap[name])
         pd_name = pd.read_csv(scrap[name])
         pd_name = pd.DataFrame(pd_name, columns=['title', 'response'])
-        pd_name = pd_name.replace(r'"', r'', regex=True)
+        #pd_name = pd_name.replace(r'["“”]', '', regex=True)    # 去除双引号
         pd_name['title'] = '0.0 '+ pd_name['title'].astype(str)
         pd_name['response'] = '1.0 '+ pd_name['response'].astype(str)
         pd_name['title'] = pd_name['title'].apply(process)
