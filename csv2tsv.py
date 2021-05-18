7e86f9e5882fa1dce24557d53c0a3eadeaab69de
西瓜 <wtmlon@foxmail.com>
2021-05-18 22:21:07 +0800

    #规则变动

--- csv2tsv.py
+++ csv2tsv.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import string
+import re
 
+def process(x):
+    x = re.sub(r'([?.,!])$',r' \1', x)
+    return x
+        
 if __name__ == '__main__':
     counsel = {
         'counsel_chat' : './Data/20200325_counsel_chat.csv',
@@ -14,8 +20,11 @@ if __name__ == '__main__':
         print(name, counsel[name])
         pd_name = pd.read_csv(counsel[name])
         pd_name = pd.DataFrame(pd_name, columns=['questionTitle', 'answerText'])
-        pd_name = pd_name.replace(r'([?.,!"])', r' \1', regex=True)
-        pd_name.columns=['0.0 question', ' 1.0 answer']
+        pd_name = pd_name.replace(r'"', r'', regex=True)
+        pd_name['questionTitle'] = '0.0 '+ pd_name['questionTitle'].astype(str)
+        pd_name['answerText'] = '1.0 '+ pd_name['answerText'].astype(str)
+        pd_name['questionTitle'] = pd_name['questionTitle'].apply(process)
+        pd_name['answerText'] = pd_name['answerText'].apply(process)
         print(pd_name)
         pd_name.to_csv('./Data/'+name+'.tsv', sep='\t', index=False)
 
@@ -23,8 +32,11 @@ if __name__ == '__main__':
         print(name, scrap[name])
         pd_name = pd.read_csv(scrap[name])
         pd_name = pd.DataFrame(pd_name, columns=['title', 'response'])
-        pd_name = pd_name.replace(r'([?.,!"])', r' \1', regex=True)
-        pd_name.columns=['0.0 question', '  1.0 answer']
+        pd_name = pd_name.replace(r'"', r'', regex=True)
+        pd_name['title'] = '0.0 '+ pd_name['title'].astype(str)
+        pd_name['response'] = '1.0 '+ pd_name['response'].astype(str)
+        pd_name['title'] = pd_name['title'].apply(process)
+        pd_name['response'] = pd_name['response'].apply(process)
         print(pd_name)
         pd_name.to_csv('./Data/'+name+'.tsv', sep='\t', index=False)
  
